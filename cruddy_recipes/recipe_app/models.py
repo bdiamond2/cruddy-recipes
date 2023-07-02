c6934cdb13d93839e2d8dbe208ba3e1be8abc77c
bdiamond2 <benarthur.diamond@gmail.com>
2023-07-02 21:50:18 +0000

    initial models

---
+++ cruddy_recipes/recipe_app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    ingredients = models.ManyToManyField(
+        'Ingredient', through='RecipeIngredient')
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    unit = models.CharField(max_length=50)
+    amount = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return f'{self.recipe} - {self.ingredient} ({self.unit} {self.amount})'
